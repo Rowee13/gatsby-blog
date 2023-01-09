e83fa114b768d49016efb054450be5c47543f548
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2023-01-09 17:23:11 +0800

    Finished Gatsby tutorial Part 4

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import Seo from "../components/seo";
 
 const AboutPage = () => {
   return (
@@ -11,6 +12,6 @@ const AboutPage = () => {
   );
 };
 
-export const Head = () => <title>About Me</title>;
+export const Head = () => <Seo title="About me" />;
 
 export default AboutPage;
