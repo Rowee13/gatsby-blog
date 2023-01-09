b3d6ae6e973683a4617cb0d09f5836430fdfd0c2
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2023-01-09 15:36:04 +0800

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,5 @@ module.exports = {
     title: `My First Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
